31d6254fa054a65a916aa01df0bd8499d3e3409c
Ben Just <benjust@gmail.com>
2018-02-14 21:26:43 +1000

    add version info

--- src/IrregularTerrainModel/Properties/AssemblyInfo.cs
+++ src/IrregularTerrainModel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IrregularTerrainModel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Irregular Terrain Model")]
+[assembly: AssemblyDescription("Longley Rice Irregular Terrain Model")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IrregularTerrainModel")]
+[assembly: AssemblyProduct("Irregular Terrain Model")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2018.2.*")]
+[assembly: AssemblyFileVersion("2018.2.0.0")]
